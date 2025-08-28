07cfaa17966914643342a0150b07fe504f8ffa68
DavideCriscuolo <davide.alcatel@gmail.com>
2025-08-28 13:37:57 +0200

    Aggiunta pagina per 404

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router";
 import DefaultLayout from "./layouts/defaultLayout";
 import Home from "./pages/Home";
 import Chisiamo from "./pages/ChiSiamo";
+import PageNotFound from "./pages/PageNotFound";
 function App() {
   return (
     <>
@@ -10,6 +11,7 @@ function App() {
           <Route element={<DefaultLayout />}>
             <Route path="/" element={<Home />} />
             <Route path="/chisiamo" element={<Chisiamo />} />
+            <Route path="/*" element={<PageNotFound />} />
           </Route>
         </Routes>
       </BrowserRouter>
